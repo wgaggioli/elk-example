a696b1764810482a672e8279fb91baa60844547b
Will Gaggioli <will@eventbrite.com>
2017-04-23 09:17:02 -0500

    Add error endpoint and basic python log formatting

--- webapp/app.py
+++ webapp/app.py
@@ -6,12 +6,19 @@ from settings import LOGGING_CONFIG
 
 app = Flask('elk_example')
 
+
 @app.route('/')
 def index():
-    app.logger.info('hi mom')
+    app.logger.info('hello world')
     return 'boogars'
 
+
+@app.route('/error')
+def error():
+    1/0
+
+
 if __name__ == "__main__":
-    app.logger.info(app.logger_name)
+    app.logger.info(app.logger_name)  # DO NOT REMOVE -- needed to init logger
     logging.config.dictConfig(LOGGING_CONFIG)
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", use_reloader=True)
--- webapp/settings.py
+++ webapp/settings.py
@@ -1,11 +1,17 @@
 LOGGING_FILE = '/var/log/app.log'
 LOGGING_CONFIG = {
     'version': 1,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
+        }
+    },
     'handlers': {
         'file': {
             'class': 'logging.handlers.RotatingFileHandler',
             'level': 'INFO',
-            'filename': LOGGING_FILE
+            'filename': LOGGING_FILE,
+            'formatter': 'default'
         }
     },
     'loggers': {
